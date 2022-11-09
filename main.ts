3bdbf8f5d6c4785288525f5777538713fe1ed85f
Jordan Reger <jordanreger@gmail.com>
2022-11-09 16:59:14 -0500

    chore: minor content updates

--- main.ts
+++ main.ts
@@ -13,14 +13,14 @@ const App = {
         "/blog": `${root}/pages/blog.html`,
 
         /* archive */
-        "/archive/blog": `${root}/pages/blog/index.html`,
-	      "/archive/blog/left": `${root}/pages/blog/left.html`,
-        "/blog/left": `${Deno.args[0] !== "localhost" ? null : "http://localhost:8000"}/archive/blog/left`,
+        "/archive/blog": `${root}/pages/archive/blog/index.html`,
+	    "/archive/blog/left": `${root}/pages/archive/blog/left.html`,
+        "/blog/left": `https://jordanreger.com/archive/blog/left`,
 
         /* resources */
         "/index.css": `${root}/static/index.css`,
         "/logo.svg": `${root}/static/logo.svg`,
-	      "/left.png": `${root}/static/left.png`,
+	    "/left.png": `${root}/static/left.png`,
         "/robots.txt": `${root}/static/robots.txt`,
 
 	/* redirects */
