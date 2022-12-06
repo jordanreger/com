58482bd806e91e10fd574032cae7a67cc4cd2600
Jordan Reger <jordanreger@gmail.com>
2022-12-05 23:07:19 -0500

    refactor: key

--- build.ts
+++ build.ts
@@ -89,7 +89,7 @@ function templating(src: string, file: string) {
 }
 
 getFiles(`site`).forEach(file => {
-  if(!file.includes("robots.txt") && !file.includes("key.pub")) {
+  if(!file.includes("robots.txt") && !file.includes("key.asc")) {
     let contents = Deno.readTextFileSync(file);
     file = file.replace(`site${slash}`, "");
     contents = templating(contents, file);
