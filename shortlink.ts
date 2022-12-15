9f64a5ac969574a65186c163c7456af289f00c3a
Jordan Reger <jordanreger@gmail.com>
2022-12-15 15:06:45 -0500

    feat: shortlink server

---
+++ shortlink.ts
@@ -0,0 +1,22 @@
+import { serve } from "https://deno.land/std@0.162.0/http/server.ts";
+
+function handler(req: Request): Response {
+  const url = new URL(req.url);
+  const route = (route:string) => { const regexRoute = new RegExp(route, "gmi"); if(regexRoute.test(path)){ return path } else { return "/" }};
+  const path = url.pathname;
+
+  if(path === "/") {
+    return Response.redirect("https://jordanreger.com", 302);
+  } else if(path === route("/post/*")) {
+    const post_number = route("/post/*")?.split("/")[2];
+    if(post_number){
+      return Response.redirect(`https://jordanreger.com/posts.html#${post_number}`, 302);
+    } else {
+      return Response.redirect("https://jordanreger.com/posts.html", 302);
+    }
+  } else {
+    return Response.redirect(`https://jordanreger.com${path}.html`, 302);
+  }
+}
+
+await serve(handler);
